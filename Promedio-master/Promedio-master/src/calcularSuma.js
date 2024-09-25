37a030a1822f25160985588d0584c402251bbb77
Edwin14459 <90294402+Edwin14459@users.noreply.github.com>
2024-09-25 14:12:39 -0600

    calcularSuma.js

--- Promedio-master/Promedio-master/src/calcularSuma.js
+++ Promedio-master/Promedio-master/src/calcularSuma.js
@@ -15,9 +15,7 @@ function calcularSumaCorregida(numeros) {
     let suma = 0;
     for (let i = 0; i < numeros.length; i++) {
         suma += numeros[i];
-
-
-
-    
-
+        }
+    return suma;
+}
 
