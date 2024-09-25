d7cf2056a91b0b0d1f12b6cf605d4391a20ea438
yanet-02 <90220423+yanet-02@users.noreply.github.com>
2024-09-25 12:56:40 -0600

    calcularSuma.js

--- Promedio-master/Promedio-master/src/calcularSuma.js
+++ Promedio-master/Promedio-master/src/calcularSuma.js
@@ -9,14 +9,4 @@ function calcularSumaDefecto(numeros) {
     return suma; // Defecto: si no se pasa un array, podría arrojar un error
 }
 
-// Versión corregida
-function calcularSumaCorregida(numeros) {
-    if (!Array.isArray(numeros)) return 0; // Manejar el caso de entrada no válida
-    let suma = 0;
-    for (let i = 0; i < numeros.length; i++) {
-        suma += numeros[i];
-    }
-    return suma;
-}
 
-module.exports = { calcularSumaDefecto, calcularSumaCorregida };
