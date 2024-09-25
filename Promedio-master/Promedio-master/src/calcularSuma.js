297e8552860966825c342f7d1489c0a5ccace18e
JeroRoque <124847927+JeroRoque@users.noreply.github.com>
2024-09-25 13:25:35 -0600

    calcularSuma.js

--- Promedio-master/Promedio-master/src/calcularSuma.js
+++ Promedio-master/Promedio-master/src/calcularSuma.js
@@ -11,6 +11,7 @@ function calcularSumaDefecto(numeros) {
 
 // Versión corregida
 function calcularSumaCorregida(numeros) {
-
+ if (!Array.isArray(numeros)) return 0; // Manejar el caso de entrada no válida
+    let suma = 0;
 
 
