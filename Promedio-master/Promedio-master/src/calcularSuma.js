dc6ce981080d6bdc00a2a55018bfee7d10a1ae7c
JeroRoque <124847927+JeroRoque@users.noreply.github.com>
2024-09-25 13:22:11 -0600

    calcularSuma.js

--- Promedio-master/Promedio-master/src/calcularSuma.js
+++ Promedio-master/Promedio-master/src/calcularSuma.js
@@ -6,7 +6,6 @@ function calcularSumaDefecto(numeros) {
     for (let i = 0; i < numeros.length; i++) {
         suma += numeros[i];
     }
-    return suma; // Defecto: si no se pasa un array, podría arrojar un error
-}
+
 
 
